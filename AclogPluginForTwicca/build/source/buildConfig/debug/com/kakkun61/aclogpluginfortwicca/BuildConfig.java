122053d62650e0c4c8f19b8e86b596449e8c89ce
岡本和樹 <kazuki@Carbon.(none)>
2013-07-15 23:52:08 +0900

    initial commit

---
+++ AclogPluginForTwicca/build/source/buildConfig/debug/com/kakkun61/aclogpluginfortwicca/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.kakkun61.aclogpluginfortwicca;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
