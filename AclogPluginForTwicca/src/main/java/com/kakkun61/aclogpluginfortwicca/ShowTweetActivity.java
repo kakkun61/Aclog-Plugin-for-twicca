e9bd61e24d1927070d2ff4655e0ddf8a5560f622
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2013-07-18 01:57:56 +0900

    固定 URL の json 取ってきて表示

--- AclogPluginForTwicca/src/main/java/com/kakkun61/aclogpluginfortwicca/ShowTweetActivity.java
+++ AclogPluginForTwicca/src/main/java/com/kakkun61/aclogpluginfortwicca/ShowTweetActivity.java
@@ -1,7 +1,59 @@
 package com.kakkun61.aclogpluginfortwicca;
 
 import android.app.Activity;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ShowTweetActivity extends Activity {
+    private TextView text;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.show_tweet);
+        text = (TextView) findViewById(R.id.text);
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    AndroidHttpClient httpClient = AndroidHttpClient.newInstance("");
+                    HttpGet get = new HttpGet("http://aclog.koba789.com/api/tweets/show.json?id=343301067234242560");
+                    HttpResponse response = httpClient.execute(get);
+                    return new BufferedReader(new InputStreamReader(response.getEntity().getContent())).readLine();
+                } catch (IOException e) {
+                    Log.w("aclog", "", e);
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                Log.d("aclog", "before download");
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String string) {
+                Log.d("aclog", "download done");
+                super.onPostExecute(string);
+                text.setText(string);
+            }
+        }.execute();
+    }
 }
